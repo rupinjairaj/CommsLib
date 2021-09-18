eddba0178278a6808578cdc3d8c43c8be37d03ff
Rupin Jairaj <rupinjairaj@gmail.com>
2021-09-17 22:10:25 -0500

    adds basic file reader code to read a config file

--- README.md
+++ README.md
@@ -9,3 +9,26 @@ Project code for **CS6378 Advanced Operating Systems** *Project 1*
 - The compiled output files will be generated in the `bin` folder by default.
 
 - This project is written using VS Code with the *Extension Pack for Java* enabled.
+
+## Configutaion file guidelines
+- Plain-text file (max 100kB).
+- Valid lines begin with unsigned integers.
+- Ignore invalid lines.
+
+### Sample config file
+<code>
+    
+    5 # number of nodes
+
+    0 dc02 12234    # nodeID hostName listeningPort
+    1 dc03 11233
+    2 dc04 22233
+    3 dc05 15232
+    4 dc06 16233
+    1 4             # space delimited list of neighbors for node 0
+    0 2 3           # space delimited list of neighbors for node 1
+    1 3             # ... node 2
+    1 2 4           # ... node 3
+    0 3             # ... node 4    
+
+</code>
--- src/utilities/Config.java
+++ src/utilities/Config.java
@@ -1,4 +1,106 @@
 package utilities;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+
 public class Config {
+    private String filePath;
+
+    public Config(String path) {
+        filePath = path;
+    }
+
+    // If the line does not begin with a number
+    // the the line is invalid.
+    private boolean isLineValid(String line) {
+        return !line.isEmpty() && Character.isDigit(line.charAt(0));
+    }
+
+    // remove comment if present in line
+    private String cleanLine(String line) {
+        if (line.indexOf('#') != -1)
+            line = line.substring(0, line.indexOf('#'));
+        return line.trim();
+    }
+
+    // get arraylist of space delimited values of each line.
+    private String[] lineItems(String line) {
+        return line.split(" ");
+    }
+
+    // helper method to get the next valid line from the file.
+    private String getNextValidLine(BufferedReader reader) {
+        String line = null;
+        try {
+            line = reader.readLine();
+            while (line != null && !isLineValid(line)) {
+                line = reader.readLine();
+            }
+            return line;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return line;
+    }
+
+    /**
+     * Reads only valid lines from the configuration file. - First valid line is the
+     * number of nodes in the system. - Next n valid lines are the details about the
+     * node: eg: 0 dc02 12234 # node ID, host name, host port. - Next n valid lines
+     * are mapping info about node and their neighbors. eg: 1 4 # space delimited
+     * list of neighbors for node 0
+     */
+    public void readFile() {
+        File f = new File(getCurrentDir() + filePath);
+        if (!f.exists() || f.isDirectory()) {
+            System.out.println("No valid file path provided for Config file.");
+
+        }
+
+        BufferedReader reader;
+        NetworkInfo networkInfo = NetworkInfo.getInstance();
+        String line;
+        try {
+            reader = new BufferedReader(new FileReader(getCurrentDir() + filePath));
+
+            // first valid line
+            line = getNextValidLine(reader);
+            System.out.println(line);
+            line = cleanLine(line);
+            int numNodes = Integer.parseInt(line);
+            networkInfo.setNumberOfNodes(numNodes);
+
+            // k valid lines hold info about nodes (i.e., node ID, host name & host port)
+            int k = numNodes;
+            while (k > 0 && line != null) {
+                k--;
+                line = getNextValidLine(reader);
+                line = cleanLine(line);
+                // TODO: map network info here.
+                String items[] = lineItems(line);
+            }
+
+            // n valid lines hold IDs of neighbors of kth node.
+            k = numNodes;
+            while (k > 0 && line != null) {
+                k--;                
+                line = getNextValidLine(reader);
+                line = cleanLine(line);
+                // TODO: map node neighbor info here
+                String items[] = lineItems(line);
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // helper method to get the dir of projects
+    private String getCurrentDir() {
+        return FileSystems.getDefault().getPath("").toAbsolutePath().toString();
+    }
 }
