68c4a4b03b31a1c56aea4cc321ac4fba26340ee6
Rupin Jairaj <rupinjairaj@gmail.com>
2021-11-07 15:58:34 -0600

    rupinjairaj/assignment2 (#5)
    * assignment 2 completed
    
    * fixed daemon server listener thread from failing to exit

--- src/Node.java
+++ src/Node.java
@@ -1,31 +1,20 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.file.FileSystems;
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Node {
     private NodeID identifier;
-
-    private int numberOfNodes;
-    public HashMap<Integer, NodeInfo> network;
-    private ArrayList<NodeID> neighbors;
     private Listener listener;
+    private int connectedNeighborCount = 0;
 
     public Node(NodeID identifier, String configFile, Listener listener) {
         this.identifier = identifier;
-        this.network = new HashMap<Integer, NodeInfo>();
-        this.neighbors = new ArrayList<NodeID>();
-        this.readFile(configFile);
+        Config.getInstance().setIdentifier(identifier);
+        Config.getInstance().readFile(configFile);
         this.listener = listener;
-        NodeListener nodeListener = new NodeListener(listener, network.get(identifier.getID()).portNumber,
-                this.neighbors, this.network);
+        NodeListener nodeListener = new NodeListener(listener, this);
         Thread listenerThread = new Thread(nodeListener, "th_serverListener");
         listenerThread.start();
 
@@ -34,28 +23,39 @@ public class Node {
     }
 
     public NodeID[] getNeighbors() {
-        return neighbors.toArray(new NodeID[0]);
+        return Config.getInstance().getNighbors();
+    }
+
+    public synchronized void incrementConnectedNeighborCount() {
+        this.connectedNeighborCount++;
+        notifyAll();
+    }
+
+    public synchronized int getConnectedNeighborCount() {
+        return this.connectedNeighborCount;
     }
 
     public void send(Message message, NodeID destination) {
-        NodeInfo dest = network.get(destination.getID());
+        NodeInfo dest = Config.getInstance().getNodeInfo(destination.getID());
         try {
             message.source = identifier;
             dest.outputStream.writeObject(message);
         } catch (Exception e) {
-            e.printStackTrace();
+            // e.printStackTrace();
         }
     }
 
     public void sendToAll(Message message) {
-        for (NodeID nId : neighbors) {
+        NodeID[] nodeIDs = Config.getInstance().getNighbors();
+        for (NodeID nId : nodeIDs) {
             send(message, nId);
         }
     }
 
     public void tearDown() {
-        for (NodeID nodeID : neighbors) {
-            NodeInfo nodeInfo = network.get(nodeID.getID());
+        NodeID[] nodeIDs = Config.getInstance().getNighbors();
+        for (NodeID nodeID : nodeIDs) {
+            NodeInfo nodeInfo = Config.getInstance().getNodeInfo(nodeID.getID());
             if (!nodeInfo.socket.isConnected())
                 continue;
             try {
@@ -63,134 +63,19 @@ public class Node {
                 nodeInfo.inputStream.close();
                 nodeInfo.socket.close();
             } catch (Exception e) {
-                e.printStackTrace();
+                // e.printStackTrace();
             }
         }
-        System.exit(1);
-    }
-
-    /**
-     * Helper methods of this class below this section
-     */
-    // Read and build network info from config file.
-
-    // add a node to 'network'.
-    public void addNode(NodeInfo node) {
-        network.put(node.id, node);
-    }
-
-    // update the node's neighbour list
-    public void addNeighbor(NodeID node) {
-        NodeInfo neighbor = network.get(node.getID());
-        // creates a socket to the neighbor node. It however
-        // does not connect to it yet. Connection is handled the
-        // first time a message is sent.
-        neighbor.socket = new Socket();
-        neighbors.add(node);
-    }
-
-    // If the line does not begin with a number
-    // the the line is invalid.
-    private boolean isLineValid(String line) {
-        return !line.isEmpty() && Character.isDigit(line.charAt(0));
-    }
-
-    // remove comment if present in line
-    private String cleanLine(String line) {
-        if (line.indexOf('#') != -1)
-            line = line.substring(0, line.indexOf('#'));
-        return line.trim();
-    }
-
-    // get arraylist of space delimited values of each line.
-    private String[] lineItems(String line) {
-        return line.split(" ");
-    }
-
-    // helper method to get the next valid line from the file.
-    private String getNextValidLine(BufferedReader reader) {
-        String line = null;
-        try {
-            line = reader.readLine();
-            while (line != null && !isLineValid(line)) {
-                line = reader.readLine();
-            }
-            return line;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return line;
-    }
-
-    /**
-     * Reads only valid lines from the configuration file. - First valid line is the
-     * number of nodes in the system. - Next n valid lines are the details about the
-     * node: eg: 0 dc02 12234 # node ID, host name, host port. - Next n valid lines
-     * are mapping info about node and their neighbors. eg: 1 4 # space delimited
-     * list of neighbors for node 0
-     */
-    public void readFile(String filePath) {
-        File f = new File(getCurrentDir() + filePath);
-        if (!f.exists() || f.isDirectory()) {
-            System.out.println("No valid file path provided for Config file.");
-            System.exit(1);
-        }
-
-        BufferedReader reader;
-        String line;
-        try {
-            reader = new BufferedReader(new FileReader(getCurrentDir() + filePath));
-
-            // first valid line
-            line = getNextValidLine(reader);
-            line = cleanLine(line);
-            this.numberOfNodes = Integer.parseInt(line);
-
-            // k valid lines hold info about nodes (i.e., node ID, host name & host port)
-            int k = this.numberOfNodes;
-            while (k > 0 && line != null) {
-                k--;
-                line = getNextValidLine(reader);
-                line = cleanLine(line);
-                String items[] = lineItems(line);
-                int id = Integer.parseInt(items[0].trim());
-                String hostName = items[1].trim();
-                int port = Integer.parseInt(items[2].trim());
-                NodeInfo node = new NodeInfo(id, hostName, port, null, null, null);
-                this.addNode(node);
-            }
-
-            // n valid lines hold IDs of neighbors of kth node.
-            k = 0;
-            while (k < this.numberOfNodes && line != null) {
-                k++;
-                line = getNextValidLine(reader);
-                if (k - 1 != this.identifier.getID())
-                    continue;
-                line = cleanLine(line);
-                String items[] = lineItems(line);
-                for (String i : items) {
-                    this.addNeighbor(new NodeID(Integer.parseInt(i)));
-                }
-            }
-
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // helper method to get the dir of projects
-    private String getCurrentDir() {
-        return FileSystems.getDefault().getPath("").toAbsolutePath().toString();
     }
 
     // connect to peers with higher IDs
     private void connectWithNeighbours() {
-        for (NodeID nodeID : neighbors) {
+        NodeID[] nodeIDs = Config.getInstance().getNighbors();
+
+        for (NodeID nodeID : nodeIDs) {
             if (nodeID.getID() < identifier.getID())
                 continue;
-            NodeInfo neighbourNode = network.get(nodeID.getID());
+            NodeInfo neighbourNode = Config.getInstance().getNodeInfo(nodeID.getID());
             // keep trying to establish a connection with
             // the peer every 5 seconds and exit the loop
             // once the connection is established.
@@ -206,21 +91,34 @@ public class Node {
                         neighbourNode.inputStream = new ObjectInputStream(socket.getInputStream());
                         Message initMessage = new Message(this.identifier, null);
                         neighbourNode.outputStream.writeObject(initMessage);
-                        network.put(neighbourNode.id, neighbourNode);
-                        PeerListener peerListener = new PeerListener(this.listener, network.get(neighbourNode.id));
+                        Config.getInstance().setNodeInfo(neighbourNode);
+                        PeerListener peerListener = new PeerListener(this.listener, neighbourNode);
                         Thread listenerThread = new Thread(peerListener, "th_nodeListener" + neighbourNode.id);
                         listenerThread.start();
+                        this.incrementConnectedNeighborCount();
                         break;
                     }
                 } catch (Exception e) {
-                    System.out.println("Failed to connect with node: " + nodeID.getID() + " Retrying in 5 seconds...");
+                    System.out.println("Failed to connect with node: " + nodeID.getID() + " Retrying in 1 second(s)...");
                     // e.printStackTrace();
                 }
                 try {
-                    Thread.sleep(5000);
+                    Thread.sleep(1000);
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    // e.printStackTrace();
                 }
+            }
+        }
+        // this block of code ensures that the Node class initialization call from
+        // the Application does not relinquish control back to the Application until
+        // a connection has been established with all its neighbors.
+        while (this.getConnectedNeighborCount() < Config.getInstance().getNighbors().length) {
+            try {
+                synchronized (this) {
+                    wait();
+                }
+            } catch (Exception e) {
+                // e.printStackTrace();
             }
         }
     }
@@ -229,21 +127,20 @@ public class Node {
 class NodeListener implements Runnable {
     private Listener listener;
     private int port;
-    private ArrayList<NodeID> neighbors;
-    public HashMap<Integer, NodeInfo> network;
+    private Node node;
+    private int lowerConns = 0;
 
-    public NodeListener(Listener listener, int port, ArrayList<NodeID> n, HashMap<Integer, NodeInfo> network) {
+    public NodeListener(Listener listener, Node node) {
         this.listener = listener;
-        this.port = port;
-        this.neighbors = n;
-        this.network = network;
+        this.port = Config.getInstance().getNodeInfo(Config.getInstance().getIdentifier().getID()).portNumber;
+        this.node = node;
     }
 
     @Override
     public void run() {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-            while (true) {
+            while (lowerConns < Config.getInstance().getLowerIDNeighborCnt()) {
                 Socket s = null;
 
                 try {
@@ -253,29 +150,31 @@ class NodeListener implements Runnable {
                     ObjectOutputStream outputStream = new ObjectOutputStream(s.getOutputStream());
                     ObjectInputStream inputStream = new ObjectInputStream(s.getInputStream());
                     Message initMessage = (Message) inputStream.readObject();
+                    NodeID[] neighbors = Config.getInstance().getNighbors();
                     for (NodeID id : neighbors) {
-                        NodeInfo nodeInfo = network.get(id.getID());
+                        NodeInfo nodeInfo = Config.getInstance().getNodeInfo(id.getID());
                         if (nodeInfo.id == initMessage.source.getID()) {
                             nodeInfo.socket = s;
                             nodeInfo.outputStream = outputStream;
                             nodeInfo.inputStream = inputStream;
-                            network.put(id.getID(), nodeInfo);
+                            Config.getInstance().setNodeInfo(nodeInfo);
                             PeerListener peerListener = new PeerListener(listener, nodeInfo);
                             Thread listenerThread = new Thread(peerListener, "th_nodeListener" + nodeInfo.id);
                             listenerThread.start();
+                            node.incrementConnectedNeighborCount();
+                            this.lowerConns++;
                         }
                     }
                 } catch (Exception e) {
                     s.close();
-                    e.printStackTrace();
+                    // e.printStackTrace();
                 }
             }
 
         } catch (IOException e) {
-            e.printStackTrace();
+            // e.printStackTrace();
         }
     }
-
 }
 
 class PeerListener implements Runnable {
@@ -301,5 +200,4 @@ class PeerListener implements Runnable {
             }
         }
     }
-
 }
--- src/Payload.java
+++ src/Payload.java
@@ -1,79 +1,71 @@
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 
 //Payload needs to be serializable in order to convert it to byte array
-class Payload implements java.io.Serializable
-{
-	//Payload may be configured as per the need of the application
+class Payload implements java.io.Serializable {
+	// Payload may be configured as per the need of the application
+	// if type is 0 -> requesting round info of roundNumber
+	// if type is 1 -> sending round info for roundNumber
+	// if type is 2 -> indicating that the node is done collecting all k-1 round
+	// info
 	int messageType;
+	int roundNumber;
+	ArrayList<Integer> nodeIDs;
 
-	//Constructor
-	public Payload(int m)
-	{
-		messageType = m;
+	// Constructor
+	public Payload(int messageType, int roundNumber, ArrayList<Integer> nodeIDs) {
+		this.messageType = messageType;
+		this.nodeIDs = nodeIDs;
+		this.roundNumber = roundNumber;
 	}
 
-	//Method to convert an instance of payload to a byte array
-	public byte[] toBytes()
-	{
-		//Output streams help with serialization
+	// Method to convert an instance of payload to a byte array
+	public byte[] toBytes() {
+		// Output streams help with serialization
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		ObjectOutputStream oos = null;
 		byte[] result = null;
-		try 
-		{
-			oos = new ObjectOutputStream(bos);   
+		try {
+			oos = new ObjectOutputStream(bos);
 			oos.writeObject(this);
 			oos.flush();
 			result = bos.toByteArray();
-		}
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("Unable to serialize payload");
-		}
-		finally 
-		{
-			try
-			{
+		} finally {
+			try {
 				bos.close();
-			}
-			catch (IOException ex) 
-			{
+			} catch (IOException ex) {
 				// ignore close exception
 			}
 		}
 		return result;
 	}
-	
-	//Method to convert a byte array to an instance of payload
-	public static Payload getPayload(byte[] data)
-	{
-		//Input streams help with deserialization
+
+	// Method to convert a byte array to an instance of payload
+	public static Payload getPayload(byte[] data) {
+		// Input streams help with deserialization
 		ByteArrayInputStream bis = new ByteArrayInputStream(data);
 		ObjectInputStream ois = null;
-		Payload p = new Payload(0);
-		try 
-		{
+		ArrayList<Integer> list = new ArrayList<>();
+		int roundNumber = 0;
+		Payload p = new Payload(0, roundNumber, list);
+		try {
 			ois = new ObjectInputStream(bis);
 			p = (Payload) ois.readObject();
-		} 
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("Unable to deserialize payload");
-		}
-		finally 
-		{
-			try 
-			{
-				if (ois != null) 
-				{
+		} finally {
+			try {
+				if (ois != null) {
 					ois.close();
 				}
 			} catch (IOException ioe) {
-			// ignore close exception
+				// ignore close exception
 			}
 		}
 		return p;
